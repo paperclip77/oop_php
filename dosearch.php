b4a29d50a5c2425bc4d1cea0763bcb4aa8c4180e
paperclip77 <leroux@paperclip.co.za>
2014-04-10 16:53:53 +0200

    second commit messages

--- dosearch.php
+++ dosearch.php
@@ -1,5 +1,12 @@
 <?php
 
 $criterion = @htmlentities($_GET["criterion"], ENT_NOQUOTES);
+if(strpos($criterion, '\"')){
+    $criterion = stripslashes($criterion);
+    echo $criterion.'<hr />';
+} else {
+    echo '\"'.$criterion.'<hr />';
+}
+
 
 ?>
--- test_feed.php
+++ test_feed.php
@@ -1,7 +1,21 @@
 <?php
 $feed = "http://z.about.com/6/g/classicalmusic/b/index.xml";
+//$feed = 'rss.xml';
+//$feed = 'classicalmusic.xml';
 $sxml = simplexml_load_file($feed);
+
+echo '<h2>'.$sxml->channel->title.'</h2>';
+
+foreach($sxml->channel->item as $item){
+    $strtemp = '<a href="'.$item->link.'">'.$item->title.'</a>';
+    $strtemp.='<p>'.$item->description.'</p>';
+    echo $strtemp;
+}
+
 foreach($sxml->attributes() as $key => $value){
     echo "RSS $key $value";
 }
+
+//API KEY: AIzaSyBV3k9JglHbJG9BnprKCHouRAY7yOIaxjs
+
 ?>
